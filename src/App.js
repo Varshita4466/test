65d6bced100eb7057acf9aacfdc0d3a71a0b9b50
Varshita4466 <94522188+Varshita4466@users.noreply.github.com>
2023-10-22 16:44:17 +0530

    task1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import WordCounter from './components/countWord';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter/> 
+    </div>
+
+  );
+}
+
+export default App;
